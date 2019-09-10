2bbc09a17a79691c061570a71a8a5cb4acb1c2b2
lottons <44310324+lottons@users.noreply.github.com>
2019-09-10 22:25:45 +0800

    Create LogHandler.py

---
+++ supplier/LogHandler.py
@@ -0,0 +1,34 @@
+import os
+
+
+class LogHandler:
+    def __init__(self):
+        self.path = 'd:\\temp\\log\\'
+
+    def logSuccessInfo(self, msg):
+        try:
+            f_success = self.path + '_success.txt'
+            if not os.path.isfile(f_success):
+                f = open(f_success, 'a')
+                f.write( msg + '\r\n')
+                f.close()
+            else:
+                f = open(f_success, 'a')
+                f.write( msg + '\r\n')
+                f.close()
+        except FileNotFoundError:
+            print( " File is not found." )
+
+    def logErrorInfo(self, msg):
+        try:
+            f_error = self.path + '_error.txt'
+            if not os.path.isfile(f_error):
+                f = open( f_error, 'a')
+                f.write( msg + '\r\n')
+                f.close()
+            else:
+                f = open(f_error, 'a')
+                f.write( msg + '\r\n')
+                f.close()
+        except FileNotFoundError:
+            print( " File is not found." )
