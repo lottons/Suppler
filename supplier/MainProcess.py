c595a32fdb653203c08db4dee4986c437c3e4cd8
lottons <44310324+lottons@users.noreply.github.com>
2019-09-21 09:43:39 +0800

    Update MainProcess.py
    更新说明：
    1. 增加扩大查询时间功能
    从当前时间开始，每次以30天为间隔，循环5次，查询6个月以内的数据
    
    2. 修改原来的处理逻辑
    由先调用短信接口发送通知短信，再调用发货接口进行发货。变更为：先调用发货接口进行发货，再调用短信接口发送通知短信。
    避免因为订单异常情况（客户退货，无标记），导致发送通知短信带来的损失。

--- supplier/MainProcess.py
+++ supplier/MainProcess.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 import time
 import schedule
@@ -8,18 +9,18 @@ from supplier.SendSMSHandler import SendSMSHandler
 from supplier.StockHandler import StockHandler
 
 
-def execute_main_command(startPage):
+def execute_main_command(startPage, startTime, endTime):
     logHandler = LogHandler()
     orderHandler = OrderHandler()
     stockHandler = StockHandler()
     sendSMSHandler = SendSMSHandler()
 
     try:
-        saleOrders = orderHandler.queryOrders(startPage)
+        saleOrders = orderHandler.queryOrders(startPage, startTime, endTime)
         print(saleOrders)
     except Exception as e:
-        print( e )
-        sys.exit( 1 )
+        print(e)
+        sys.exit(1)
     if saleOrders is None:
         print("没有查询到订单")
         return False
@@ -44,29 +45,32 @@ def execute_main_command(startPage):
                     print(resId)
                     if resId:
                         try:
-                            result = sendSMSHandler.sendSMS(customerName,mobilePhone, productCode, resId )
-                            if result != True:
-                                logHandler.logErrorInfo(
-                                    "订单" + orderId + "发送短信卡密失败，调用接口失败")
-                            else:
-                                print("订单" + orderId + "发货卡密成功")
-                        except Exception as e:
-                            print( e )
-                            logHandler.logErrorInfo(
-                                "订单" + orderId + "发送短信卡密失败 :" + e )
-                            sys.exit( 1 )
-                        try:
                             if count == float(saleQty) - 1:
                                 result = orderHandler.orderDeliver( orderItemId )
                                 if result != True:
                                     logHandler.logErrorInfo(
-                                        "订单" + orderId + "发货失败，调用接口失败。" )
+                                        "订单" + orderId + "发货失败，调用苏宁发货接口失败。 产品编码：" +  productCode + " | 卡密：" + resId)
+                                    break
                                 else:
                                     print("订单" + orderId + "发货成功")
                         except Exception as e:
                             print(e)
                             logHandler.logErrorInfo(
                                 "订单" + orderId + "发货失败 :" + e)
+                            sys.exit(1)
+
+                        try:
+                            result = sendSMSHandler.sendSMS(customerName,mobilePhone, productCode, resId )
+                            if result != True:
+                                logHandler.logErrorInfo(
+                                    "订单" + orderId + "发送短信卡密失败，调用短信发送接口失败。产品编码：" +  productCode + " | 卡密：" + resId)
+                                break
+                            else:
+                                print("订单" + orderId + "发货卡密成功")
+                        except Exception as e:
+                            print( e )
+                            logHandler.logErrorInfo(
+                                "订单" + orderId + "发送短信卡密失败 :" + e )
                             sys.exit( 1 )
 
                         print( "订单" + orderId + "已处理， 发送的卡密：" + productCode + " | " + resId )
@@ -75,24 +79,37 @@ def execute_main_command(startPage):
                         print( "订单" + orderId + "未处理，因为 " + productCode + " | " + productName + " 的卡密已用完" )
                         logHandler.logErrorInfo(
                             "订单" + orderId + "未处理，因为 " + productCode + " | " + productName + " 的卡密已用完" )
+                        break
 
                     count = count + 1
         return True
 
+
 # 被周期性调度触发的函数
 def print_time():
-    print("now is", time.strftime('%Y-%m-%d %H:%M:%S'), "开始执行任务：" )
-    startPage = 1
-    # execute_main_command(startPage)
-    flag = True
-    while flag:
-        flag = execute_main_command(startPage)
-        startPage = startPage + 1
+    print("now is", time.strftime('%Y-%m-%d %H:%M:%S'), "开始执行任务：")
+
+    # 查询订单的起始和结束时间
+    nowTime = datetime.datetime.now()
+    # zeroToday = nowTime - datetime.timedelta(hours=nowTime.hour, minutes=nowTime.minute, seconds=nowTime.second,microseconds=nowTime.microsecond)
+
+    for i in range(4):
+        startPage = 1
+        # endTime = nowTime.strftime("%Y-%m-%d %H:%M:%S")
+        endDay = i * 30
+        startDay = (i + 1) * 30
+        endTime = (nowTime - datetime.timedelta(days=endDay)).strftime("%Y-%m-%d %H:%M:%S")
+        startTime = (nowTime - datetime.timedelta(days=startDay)).strftime("%Y-%m-%d %H:%M:%S")
+        flag = True
+        print("查询开始时间 :", startTime)
+        print("查询结束时间 :", endTime)
+        while flag:
+            flag = execute_main_command(startPage, startTime, endTime)
+            startPage = startPage + 1
 
 
 if __name__ == "__main__":
-    # print('start')
-    schedule.every(5).minutes.do(print_time)
+    schedule.every(1).minutes.do(print_time)
     while True:
         schedule.run_pending()
         time.sleep(1)
