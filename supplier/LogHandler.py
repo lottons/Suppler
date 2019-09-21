2d8ee61635147b65be8b21899ff1f5bef7c90987
lottons <44310324+lottons@users.noreply.github.com>
2019-09-21 09:45:28 +0800

    Update LogHandler.py
    新增功能
    1. 每天生成一个成功和失败的文件

--- supplier/LogHandler.py
+++ supplier/LogHandler.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 
@@ -10,25 +11,25 @@ class LogHandler:
             f_success = self.path + '_success.txt'
             if not os.path.isfile(f_success):
                 f = open(f_success, 'a')
-                f.write( msg + '\r\n')
+                f.write(msg + '')
                 f.close()
             else:
                 f = open(f_success, 'a')
-                f.write( msg + '\r\n')
+                f.write(msg + '')
                 f.close()
         except FileNotFoundError:
-            print( " File is not found." )
+            print(" File is not found.")
 
     def logErrorInfo(self, msg):
         try:
             f_error = self.path + '_error.txt'
             if not os.path.isfile(f_error):
-                f = open( f_error, 'a')
-                f.write( msg + '\r\n')
+                f = open(f_error, 'a')
+                f.write(msg + '\n')
                 f.close()
             else:
                 f = open(f_error, 'a')
-                f.write( msg + '\r\n')
+                f.write(msg + '\n')
                 f.close()
         except FileNotFoundError:
-            print( " File is not found." )
+            print(" File is not found.")
